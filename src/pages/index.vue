7623dd7f6635dd55c58445a770405fd614aa2702
Meldencio Czarlemagne <mczvc@proton.me>
2025-09-13 00:02:01 +0800

    add style animations

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,14 +2,14 @@
   <main>
   <transition :name="backgroundAnimName">
     <div class="main" v-show="okEnter">
-      <NuxtLink v-for="locale in otherLocales" 
+      <NuxtLink v-for="(locale, n) in otherLocales" 
       :to="switchLocalePath(locale.code)" 
       :key="locale.code"
       class="language-selector__wrapper text-yellow-600">
-        <div class="language-selector">{{ locale.name }}</div>
+        <div class="language-selector" :data-anim-order="n">{{ locale.name }}</div>
       </NuxtLink>
-      <h1 class="mt-26">
-        <span class="text-yellow">Welcome</span> <span class="inline-block hover:scale-[1.4] hover:translate-10 transition-[transform]">{{ $t("k9gb") }} {{ $t("member") }}!</span>
+      <h1 class="heading-welcome mt-26">
+        <span class="text-yellow">Welcome</span> <span class="member-text inline-block hover:scale-[1.4] hover:translate-10" ref="memberTextEl">{{ $t("k9gb") }} {{ $t("member") }}!</span>
       </h1>
       <div class="content">
         <div class="spacer"></div>
@@ -21,9 +21,24 @@
 </template>
 
 <script setup lang="ts">
+const mainEl = ref<HTMLElement>();
 
 definePageMeta({ 
-  layout: "индекс"
+  layout: "индекс",
+  pageTransition: {
+    onAfterEnter: () => {
+      const mainEl = document.querySelector('.main');
+      lessCSSClass(mainEl!, 'delay-animation')
+      setTimeout( () => {
+        addCSSClass('.heading-welcome', 'slide-from-right');
+        addCSSClass(mainEl!, 'delay-animation');
+
+        setTimeout( () => {
+          lessCSSClass('.heading-welcome', 'slide-from-right');
+        }, 1600);
+      }, 100);
+    }
+  }
 });
 
 useHead({
@@ -39,6 +54,7 @@ const localePath = useLocalePath();
 const otherLocales = computed(() => {
   return locales.value.filter(i => i.code !== locale.value)
 });
+
 
 
 const backgroundAnimName = ref('');
@@ -58,6 +74,20 @@ const onAboutLinkClick = async () => {
 
 onMounted(() => {
   document.querySelector('.main')?.classList.add('delay-animation');
+
+  const languages = document.querySelectorAll('.language-selector');
+
+  languages.forEach( (lang) => {
+  
+    (lang as HTMLElement).style.transitionProperty = 'transform, opacity';
+    (lang as HTMLElement).style.transitionDelay = `${Number.parseInt(lang.attributes.getNamedItem('data-anim-order')?.textContent || "0") * 500 + 1500}ms`;
+
+    setTimeout(() => {
+      (lang as HTMLElement).style.transitionDelay = `unset`;
+    }, 2000);
+  
+
+  });
 });
 
 </script>
@@ -91,10 +121,18 @@ main {
   transform: scale(2.0) rotateY(180deg);
   transition: transform ease 1s;
   
+    
   h1 {
-    transition: transform cubic-bezier(0.075, 0.82, 0.165, 1) 1s 1s;
+    transition: transform cubic-bezier(0.075, 0.82, 0.165, 1) 1s 1s, opacity ease 700ms;
     transform: scale(0.5);
+    opacity: 0.1;
   
+  }
+  
+  .heading-welcome {
+    &.slide-from-right {
+      transform: translateX(100%);
+    }
   }
   
   &.delay-animation {
@@ -102,7 +140,38 @@ main {
     
     h1 {
       transform: scale(1.0);
+      
+      opacity: 1.0;
     }
+    
+    .language-selector {
+      --t-scale: 1.0;
+      transform: translate3d(0, 0, 0) scale(var(--t-scale));
+      
+      &:hover {
+        --t-scale: 1.2;
+        transition-delay: 0ms !important;
+      }
+      
+      opacity: 1;
+    }
+
+    .member-text {
+      transition: transform cubic-bezier(0.075, 0.82, 0.165, 1) 1s, color cubic-bezier(0.165, 0.84, 0.44, 1) 2s 1.7s;
+      color: #ffad00;
+    }
+  }
+
+  .language-selector {
+    transition: transform cubic-bezier(0.075, 0.82, 0.165, 1) 1s 1s, opacity ease 1s 2.5s;
+    
+    transform: translateX(50%) translateY(-100%);
+    
+    opacity: 0;
+  }
+
+  .member-text {
+    color: white;
   }
 }
 
