0f4a8f286fdd6b4d869d30b1fb6a0250080f95b8
Meldencio Czarlemagne <mczvc@proton.me>
2025-09-13 23:43:13 +0800

    merge branch

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -70,6 +70,7 @@ const icons = [
   getSrc('/assets/images/BC13.svg'),
   getSrc('/assets/images/YouMeKa-Flower.svg'),
   getSrc('/assets/images/YG.png'),
+
   getSrc('/assets/images/mczvc-web-services.svg'),
 ];
 const iconSlides = ref(icons[0]);
