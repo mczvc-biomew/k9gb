a608d30c85585e9af5c90d0cb1dabb7a70dc4cbe
Meldencio Czarlemagne <mczvc@proton.me>
2025-09-15 05:52:19 +0800

    [0.0.2] 09-15-2025 (0552H)

---
+++ .output/public/_nuxt/KjYtqqSl.js
@@ -0,0 +1,1 @@
+import s from"./CsAzoPoO.js";import{p as o,l as p,R as u,F as f}from"./BCjj4UMd.js";import"./BmlbPuZL.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=p(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
